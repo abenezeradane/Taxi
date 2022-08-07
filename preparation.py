f769d20c35d02ed9a60a8c4f9e1692154b1ecdf5
PB020 <abe05081995@gmail.com>
2022-08-07 18:53:00 -0400

    Implemented "strip_address"

--- preparation.py
+++ preparation.py
@@ -2,3 +2,24 @@ import re
 import spacy
 import pandas
 pandas.set_option("display.max_colwidth", None)
+
+def strip_address(address: str) -> str:
+    """Strips the address string of unnecessary symbols and properly formats
+        the address into a csv file style format using regex
+
+    Parameters
+    ----------
+    address: str
+        String containing the address
+
+    Returns
+    ----------
+    str
+        Properly formatted address string
+    """
+
+    stripped = re.sub(r"(,)(?!\s)", ", ", address)
+    stripped = re.sub(r"(\\n)", ", ", stripped)
+    stripped = re.sub(r"(?!\s)(-)(?!\s)", " - ", stripped)
+    stripped = re.sub(r"\.", "", stripped)
+    return stripped
