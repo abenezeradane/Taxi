d72a78ac89e283715bcbe98fb5cf2c1f6bf898ec
PB020 <abe05081995@gmail.com>
2022-08-07 18:59:40 -0400

    Implemented address_span

--- preparation.py
+++ preparation.py
@@ -23,3 +23,30 @@ def strip_address(address: str) -> str:
     stripped = re.sub(r"(?!\s)(-)(?!\s)", " - ", stripped)
     stripped = re.sub(r"\.", "", stripped)
     return stripped
+
+def address_span(address: str = None, component: str = None, label: str = None) -> tuple:
+    """Return a tuple containing the span of the address component in the
+       address string and the classification label of the component
+
+    Parameters
+    ----------
+    address: str
+        String containing the address
+    component: str
+        String containing the address component
+    label: str
+        String containing the classification label of the address component
+
+    Returns
+    ----------
+    str
+        Properly formatted address string
+    """
+
+    if pandas.isna(component) or (str(component) == 'nan'):
+        pass
+    else:
+        span = re.sub("\.", '', component)
+        span = re.sub(r"(?!\s)(-)(?!\s)", " - ", span)
+        span = re.search("\\b(?:" + span + ")\\b", address)
+        return (span.start(), span.end(), label)
