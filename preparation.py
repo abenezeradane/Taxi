7f9595fcd175cd65d9d52016af60aa87d6cea0a5
PB020 <abe05081995@gmail.com>
2022-08-07 20:17:03 -0400

    Started implementation for "create_entity_spans"

--- preparation.py
+++ preparation.py
@@ -22,7 +22,7 @@ def extend(list: list, item: str) -> list:
 
     if pandas.isna(item) or (str(item) == 'nan'):
         return list
-    else
+    else:
         list.append(item)
         return list
 
@@ -75,3 +75,33 @@ def address_span(address: str = None, component: str = None, label: str = None) 
         span = re.sub(r"(?!\s)(-)(?!\s)", " - ", span)
         span = re.search("\\b(?:" + span + ")\\b", address)
         return (span.start(), span.end(), label)
+
+def create_entity_spans(dataset: pandas.core.frame.DataFrame, tags: list) -> pandas.core.series.Series:
+    """Create a pandas Series with entity spans for the training dataset
+
+    Parameters
+    ----------
+    dataset: pandas.core.frame.DataFrame
+        pandas DataFrame containing the training dataset
+    tags: list
+        List of data tags
+
+    Returns
+    ----------
+    pandas.core.series.Series
+        pandas Series of training dataset entity spans
+    """
+
+    dataset["Address"] = dataset["Address"].apply(lambda address: strip_address(address))
+    #...
+    dataset["EmptySpan"] = dataset.apply(lambda x: [], axis=1)
+
+    for tag in tags:
+        dataset["EntitySpans"] = dataset.apply(lambda row: extend(row["EmptySpan"], row[tag]), axis=1)
+        dataset["EntitySpans"] = dataset[["EntitySpans", "Address"]].apply(lambda entity: (entity[1], entity[0]), axis=1)
+    return dataset["EntitySpans"]
+
+tags = ["Recipient", "Building_Name", "Building_Number", "Street_Name", "City", "State", "Zip_Code", "Country"]
+dataset = pandas.read_csv(filepath_or_buffer="./data/datasets/us-train-dataset.csv", sep=",", dtype=str)
+entity_spans = create_entity_spans(dataset.astype(str), tags)
+print(f"Type: {type(entity_spans)}")
